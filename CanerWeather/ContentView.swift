55460864702a0873cffd94bb74e35b6ed5d53527
yeenjie123456 <62682810+yeenjie123456@users.noreply.github.com>
2024-05-04 00:21:27 +0800

    Update mach o type and some other build settings
    1

--- CanerWeather/ContentView.swift
+++ CanerWeather/ContentView.swift
@@ -86,7 +86,7 @@ struct ContentView: View {
                         .font(.footnote)
                     Text("Version 0.0.3. Built with Swift & SwiftUI.")
                         .font(.footnote)
-                    Text(" Coded by Kent Ye. Built by Scott Xia.")
+                    Text(" Coded by Kent Ye. Built by Xcode Cloud.")
                         .font(.footnote)
                     Text("© Caner HK 2024 - All Rights Reserved.")
                         .font(.footnote)
